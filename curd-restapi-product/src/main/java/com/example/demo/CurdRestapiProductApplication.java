84f8cc66c66e9f88f2fab6eb7a63dee0727ab964
ADMIN <ADMIN@DESKTOP-S5B6Q0D.bbrouter>
2026-03-22 17:51:36 +0530

    testingg jus

---
+++ curd-restapi-product/src/main/java/com/example/demo/CurdRestapiProductApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdRestapiProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdRestapiProductApplication.class, args);
+	}
+
+}
+//mmmm
