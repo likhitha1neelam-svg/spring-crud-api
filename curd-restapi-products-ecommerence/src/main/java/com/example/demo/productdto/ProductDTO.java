c74417df496d5f9e77fc5b364c74d8aa3a7a0e0f
ADMIN <ADMIN@DESKTOP-S5B6Q0D.bbrouter>
2026-03-22 23:20:35 +0530

    fix(dev):CURD1: COMPLETE PRODUCT CURD REST API ECOMMERCE PROJECT

--- curd-restapi-products-ecommerence/src/main/java/com/example/demo/productdto/ProductDTO.java
+++ curd-restapi-products-ecommerence/src/main/java/com/example/demo/productdto/ProductDTO.java
@@ -1,5 +1,31 @@
 package com.example.demo.productdto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
+//validation dependency should add in pom.xml
+
 public class ProductDTO {
+	
+	@NotBlank(message ="name is required")
+	
+	private String name;
+	
+	@Positive(message =" price should be positive")
+	private Double price;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
 
 }
--- curd-restapi-products-ecommerence/src/main/java/com/example/demo/productexceptionhandling/ProductGlobalExceptionHandlier.java
+++ curd-restapi-products-ecommerence/src/main/java/com/example/demo/productexceptionhandling/ProductGlobalExceptionHandlier.java
@@ -1,5 +1,23 @@
 package com.example.demo.productexceptionhandling;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class ProductGlobalExceptionHandlier {
+	
+	@ExceptionHandler(ProductCustomException.class)
+	public ResponseEntity<String> handleProductNotFound(ProductCustomException ex){
+		return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
+		
+	}
+	
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handleGenericException(Exception ex ){
+		return new ResponseEntity<>("Internal server error"+ ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 
 }
