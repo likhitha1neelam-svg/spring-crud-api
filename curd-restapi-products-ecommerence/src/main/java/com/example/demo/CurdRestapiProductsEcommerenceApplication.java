974e87158aa07db7fdef51e3cc7115ccec43e3fa
ADMIN <ADMIN@DESKTOP-S5B6Q0D.bbrouter>
2026-03-22 18:07:56 +0530

    Pushing changes for testing

---
+++ curd-restapi-products-ecommerence/src/main/java/com/example/demo/CurdRestapiProductsEcommerenceApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdRestapiProductsEcommerenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdRestapiProductsEcommerenceApplication.class, args);
+	}
+
+}
+
+
+//testing
